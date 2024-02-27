ddfc5124761ffe0faec03562b1a566567d4ff4c9
Evllooew <evllooev@icloud.com>
2024-02-27 04:09:43 +0300

    исправил

---
+++ src/main/java/ru/hogwarts/school5/School5Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school5;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinxition
+public class School5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(School5Application.class, args);
+	}
+}
